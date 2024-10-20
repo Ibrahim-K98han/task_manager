9d74b102c7de0b2e9c757f6e669f7cd984a79ed5
unknown <ibrahimkhan.dev405@gmail.com>
2024-10-20 21:21:20 +0600

    signup api integration done

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -4,11 +4,44 @@ import 'package:http/http.dart';
 import 'package:task_manager/data/models/network_response.dart';
 
 class NetworkCaller {
-  static Future<NetworkResponse> getRequest(String url) async {
+  static Future<NetworkResponse> getRequest({required String url}) async {
     try {
       Uri uri = Uri.parse(url);
       debugPrint(url);
       final Response response = await get(uri);
+      printResponse(url, response);
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body);
+        return NetworkResponse(
+          isSuccess: true,
+          statusCode: response.statusCode,
+          responseData: decodedData,
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static Future<NetworkResponse> postRequest(
+      {required String url, Map<String, dynamic>? body}) async {
+    try {
+      Uri uri = Uri.parse(url);
+      debugPrint(url);
+      final Response response = await post(
+        uri,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+      );
       printResponse(url, response);
       if (response.statusCode == 200) {
         final decodedData = jsonDecode(response.body);
--- lib/ui/screens/sign_up_screen.dart
+++ lib/ui/screens/sign_up_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:task_manager/ui/screens/sign_in_screen.dart';
+import 'package:task_manager/data/models/network_response.dart';
+import 'package:task_manager/data/services/network_caller.dart';
+import 'package:task_manager/data/utils/urls.dart';
 import 'package:task_manager/ui/utils/app_colors.dart';
+import 'package:task_manager/ui/widgets/center_circular_progress_indicator.dart';
 import 'package:task_manager/ui/widgets/screen_background.dart';
+import 'package:task_manager/ui/widgets/snack_bar_message.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -12,6 +16,13 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailETController = TextEditingController();
+  final TextEditingController _firstNameETController = TextEditingController();
+  final TextEditingController _lastNameETController = TextEditingController();
+  final TextEditingController _mobileETController = TextEditingController();
+  final TextEditingController _passwordETController = TextEditingController();
+  bool _inProgress = false;
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
@@ -55,45 +66,87 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   Widget _buildSignUpForm() {
-    return Column(
-      children: [
-        TextFormField(
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration(hintText: 'Email'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(hintText: 'First Name'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(hintText: 'Last Name'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          keyboardType: TextInputType.number,
-          decoration: const InputDecoration(hintText: 'Mobile'),
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        TextFormField(
-          decoration: const InputDecoration(hintText: 'Password'),
-        ),
-        const SizedBox(
-          height: 24,
-        ),
-        ElevatedButton(
-          onPressed: _onTapNextButton,
-          child: const Icon(Icons.arrow_circle_right_outlined),
-        ),
-      ],
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _emailETController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: 'Email'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter Valid Email';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            controller: _firstNameETController,
+            decoration: const InputDecoration(hintText: 'First Name'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter First Name';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            controller: _lastNameETController,
+            decoration: const InputDecoration(hintText: 'Last Name'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter Last Name';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            controller: _mobileETController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(hintText: 'Mobile'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter Mobile Number';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            controller: _passwordETController,
+            decoration: const InputDecoration(hintText: 'Password'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Enter Password';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          Visibility(
+            visible: !_inProgress,
+            replacement: const CenterCircularProgressIndicator(),
+            child: ElevatedButton(
+              onPressed: _onTapNextButton,
+              child: const Icon(Icons.arrow_circle_right_outlined),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -121,14 +174,55 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   void _onTapNextButton() {
-    // TODO: implement on tap next button
+    if (_formKey.currentState!.validate()) {
+      _signUp();
+    }
   }
-  void _onTapSignIn() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => const SignInScreen(),
-      ),
+
+  Future<void> _signUp() async {
+    _inProgress = true;
+    setState(() {});
+    Map<String, dynamic> requestBody = {
+      "email": _emailETController.text.trim(),
+      "firstName": _firstNameETController.text.trim(),
+      "lastName": _lastNameETController.text.trim(),
+      "mobile": _mobileETController.text.trim(),
+      "password": _passwordETController.text,
+      "photo": ""
+    };
+    NetworkResponse response = await NetworkCaller.postRequest(
+      url: Urls.registration,
+      body: requestBody,
     );
+    _inProgress = false;
+    setState(() {});
+    if (response.isSuccess) {
+      _clearTextFields();
+      showSnackBarMessage(context, 'New user created');
+    } else {
+      showSnackBarMessage(context, response.errorMessage, true);
+    }
+  }
+
+  void _clearTextFields() {
+    _emailETController.clear();
+    _firstNameETController.clear();
+    _lastNameETController.clear();
+    _mobileETController.clear();
+    _passwordETController.clear();
+  }
+
+  void _onTapSignIn() {
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _emailETController.dispose();
+    _firstNameETController.dispose();
+    _lastNameETController.dispose();
+    _mobileETController.dispose();
+    _passwordETController.dispose();
+    super.dispose();
   }
 }
