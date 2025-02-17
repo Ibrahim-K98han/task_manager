0e22d9e4141875e2c02819b5f9d47a5671061cdb
unknown <ibrahimkhan.dev405@gmail.com>
2025-02-17 17:01:29 +0600

    signin and singup screen done

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screens/splash_screen.dart';
+import 'package:task_manager/utils/app_colors.dart';
 
-class TaskMangerApp extends StatelessWidget {
+class TaskMangerApp extends StatefulWidget {
   const TaskMangerApp({super.key});
 
   @override
+  State<TaskMangerApp> createState() => _TaskMangerAppState();
+}
+
+class _TaskMangerAppState extends State<TaskMangerApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: AppColors.themeColor,
+        textTheme: TextTheme(),
+        inputDecorationTheme: _inputDecorationTheme(),
+        elevatedButtonTheme: _elevatedButtonThemeData(),
+      ),
       home: SplashScreen(),
+    );
+  }
+
+  ElevatedButtonThemeData _elevatedButtonThemeData() {
+    return ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.themeColor,
+        foregroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12,
+        ),
+        fixedSize: Size.fromWidth(double.maxFinite),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+
+  InputDecorationTheme _inputDecorationTheme() {
+    return InputDecorationTheme(
+      fillColor: Colors.white,
+      filled: true,
+      border: _inputBorder(),
+      enabledBorder: _inputBorder(),
+      focusedBorder: _inputBorder(),
+      errorBorder: _inputBorder(),
+    );
+  }
+
+  OutlineInputBorder _inputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.grey),
+      borderRadius: BorderRadius.circular(8),
     );
   }
 }
