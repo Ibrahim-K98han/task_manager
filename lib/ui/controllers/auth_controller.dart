8ed3048c2d3db5ad5ef8a96f55e3a5c8704553d4
unknown <ibrahimkhan.dev405@gmail.com>
2025-03-05 11:09:21 +0600

    update profile

--- lib/ui/controllers/auth_controller.dart
+++ lib/ui/controllers/auth_controller.dart
@@ -1,12 +1,28 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager/data/models/user_model.dart';
 
 class AuthController {
   static final String _accessTokenKey = 'access-token';
+  static final String _userDataKey = 'user-data';
   static String? accessToken;
+  static UserModel? userData;
   static Future<void> saveAccessToken(String token) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.setString(_accessTokenKey, token);
     accessToken = token;
+  }
+
+  static Future<void> saveUserData(UserModel userModel) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString(
+      _userDataKey,
+      jsonEncode(
+        userModel.toJson(),
+      ),
+    );
+    userData = userModel;
   }
 
   static Future<String?> getAccessToken() async {
@@ -14,6 +30,17 @@ class AuthController {
     String? token = sharedPreferences.getString(_accessTokenKey);
     accessToken = token;
     return token;
+  }
+
+  static Future<UserModel?> getUserData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? userEncodedData = sharedPreferences.getString(_userDataKey);
+    if (userEncodedData == null) {
+      return null;
+    }
+    UserModel userModel = UserModel.fromJson(jsonDecode(userEncodedData));
+    userData = userModel;
+    return userModel;
   }
 
   static bool isLoggedIn() {
